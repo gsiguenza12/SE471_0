e515007fdd8e9f33f9b510206144736e319d4378
Gabriel Alfredo Siguenza <gebsiguenza@gmail.com>
2022-02-01 19:21:13 -0800

    created method to randomize colors on Graphics object.

--- src/LineDrawing/LiningPanel.java
+++ src/LineDrawing/LiningPanel.java
@@ -16,58 +16,39 @@ public class LiningPanel extends javax.swing.JPanel {
 
     LiningPanel() { }
 
-    public void paintComponent(Graphics g)
-    {
-        Random randNum = new Random();
+    public void paintComponent(Graphics g) {
+
 
         //**RGB components(values are in range 0 – 255)
-        int red,green,blue;
-        red = randNum.nextInt(256);
-        green = randNum.nextInt(256);
-        blue = randNum.nextInt(256);
 
         super.paintComponent(g);
-        g.setColor(new Color(red,green,blue));
         int w = getWidth();
         int h = getHeight();
 
         double lines = 15.0; // ** how spread out the lines are?
 
         // Draw line design on bottom left portion of the screen
-        for(int i = 0; i < lines; i++) // ** this for loop draws the graph design in the bottom left portion
+        for (int i = 0; i < lines; i++) // ** this for loop draws the graph design in the bottom left portion
         {
-            int w2 = (int)((i/lines)*w);
-            int h2 = (int)((i/lines)*h);
+            int w2 = (int) ((i / lines) * w);
+            int h2 = (int) ((i / lines) * h);
 
-            g.drawLine(0,  h2, w2, h); // 0 and h determine the geometry of the lines, use to reorient
+            generateRGB(g);
+            g.drawLine(0, h2, w2, h); // 0 and h determine the geometry of the lines, use to reorient
+            g.drawLine(w2, h, w, h - h2);
+            g.drawLine(w, h2, w2, 0);
+            g.drawLine(w2, 0, 0, h - h2);
         }
+      }
 
-          // Draw line design on bottom right portion of the screen
-//        for(int i = 0; i < lines; i++) // ** this for loop draws the graph design in the bottom left portion
-//        {
-//            int w2 = (int)((i/lines)*w);
-//            int h2 = (int)((i/lines)*h);
-//
-//            g.drawLine(0,  h2, w2, h);
-//        }
-//
-          // Draw line design on top right portion of the screen
-//        for(int i = 0; i < lines; i++) // ** this for loop draws the graph design in the bottom left portion
-//        {
-//            int w2 = (int)((i/lines)*w);
-//            int h2 = (int)((i/lines)*h);
-//
-//            g.drawLine(0,  h2, w2, h);
-//        }
-//
-          // Draw line design on top left portion of the screen
-//        for(int i = 0; i < lines; i++) // ** this for loop draws the graph design in the bottom left portion
-//        {
-//            int w2 = (int)((i/lines)*w);
-//            int h2 = (int)((i/lines)*h);
-//
-//            g.drawLine(0,  h2, w2, h);
-//        }
+        protected void generateRGB(Graphics g){
+            Random randNum = new Random();
+            int red, green, blue;
+            red = randNum.nextInt(256);
+            green = randNum.nextInt(256);
+            blue = randNum.nextInt(256);
+
+            g.setColor(new Color(red, green, blue));
     }
 
 }
